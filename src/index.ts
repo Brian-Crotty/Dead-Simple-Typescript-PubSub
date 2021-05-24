916d3d5a9b35c07880f800623d41fa9cf98f5eb8
Brian Crotty <5673408+Brian-Crotty@users.noreply.github.com>
2021-05-24 16:53:00 -0500

    Version 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-export class PubSub<validParams> {
-	private eventSubscribers: Set<(arg: validParams) => void> = new Set();
-	subscribe(fn: (event: validParams) => void) {this.eventSubscribers.add(fn);}
-	unsubscribe(fn: (event: validParams) => void) {this.eventSubscribers.delete(fn);}
-	publish(event: validParams) {return this.eventSubscribers.forEach((fn) => fn(event));}
+class PubSub<validParams> {
+	protected readonly eventSubscribers: Set<(arg: validParams) => void> = new Set();
+
+	subscribe(fn: (event: validParams) => any): void {
+		this.eventSubscribers.add(fn);
+	}
+
+	unsubscribe(fn: (event: validParams) => any): void {
+		this.eventSubscribers.delete(fn);
+	}
+
+	publish(event: validParams): void {
+		this.eventSubscribers.forEach((fn) => fn(event));
+	}
 }
+export { PubSub };
