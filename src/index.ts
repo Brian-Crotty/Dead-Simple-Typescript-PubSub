88ab523ab8029238734483403895be027013252b
Brian Crotty <5673408+Brian-Crotty@users.noreply.github.com>
2021-05-25 19:07:49 -0500

    Add jsdoc keep comments

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
+/** A type safe implementation of the observer pattern
+ * @type  {validParams} the type that the pubsub will handle
+ */
 class PubSub<validParams> {
-	protected readonly eventSubscribers: Set<(arg: validParams) => void> = new Set();
+	readonly eventSubscribers: Set<(arg: validParams) => unknown> = new Set();
 
-	subscribe(fn: (event: validParams) => any): void {
+	/** Adds a function that accepts the pubsub parameters to the subscriber list
+	 * @param  {(event:validParams)=>unknown} fn
+	 * @returns {Function} unsubscribe function
+	 */
+	subscribe(fn: (event: validParams) => unknown): () => void {
 		this.eventSubscribers.add(fn);
+		return () => {
+			this.unsubscribe(fn);
+		};
 	}
 
-	unsubscribe(fn: (event: validParams) => any): void {
-		this.eventSubscribers.delete(fn);
+	/** Unsubscribes the given function from the pubsub
+	 * @param  {(event:validParams)=>unknown} fn a function that was subscirbed to this pubsub
+	 * @returns {boolean} Deletion success status
+	 */
+	unsubscribe(fn: (event: validParams) => unknown): boolean {
+		return this.eventSubscribers.delete(fn);
 	}
 
+	/** Updates each of the subscribed functions with the published event
+	 * @param  {validParams} event
+	 * @returns void
+	 */
 	publish(event: validParams): void {
 		this.eventSubscribers.forEach((fn) => fn(event));
 	}
